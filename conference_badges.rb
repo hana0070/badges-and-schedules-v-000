1cffb15608c1e3906fe987d38273b845ec10e69c
HANA LEANDER <hana0070@gmail.com>
2019-04-01 13:25:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,8 @@ end
 
 def assign_rooms(attendees, room_assignments)
   attenddes.map do |attendee|
-    ""
-
+    "Hello, #{attendee}! You'll be assigned to room #{room_assignments}!"
+    room = 1 
+    room += 1
+  end
 end
